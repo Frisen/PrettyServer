c2a5fcbbf27c7b95be425142ef4622b5f8ce7c02
zhangxinshuai <945456439@qq.com>
2017-05-11 10:46:16 +0800

    first commit

---
+++ WeChatWebService/WebService1.asmx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace WeChatWebService
+{
+    /// <summary>
+    /// WebService1 的摘要说明
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+       
+        [WebMethod(Description = "GetProducts")]
+        public void GetProduct()
+        {
+            //HttpFileCollection request = Context.Request.Files;
+            //HttpPostedFile file = request[0];
+            
+            string pathSource = @"C:\cc.jpg";
+            string pathNew = @"D:\aa.jpg";
+
+            try
+            {
+
+                //using (Stream fsSource =file.InputStream)
+                using(FileStream fsSource=new FileStream (pathSource,FileMode.Open,FileAccess.Read))
+                {
+
+                    // Read the source file into a byte array.
+                    byte[] bytes = new byte[fsSource.Length];
+                    int numBytesToRead = (int)fsSource.Length;
+                    int numBytesRead = 0;
+                    while (numBytesToRead > 0)
+                    {
+
+                        // Read may return anything from 0 to numBytesToRead.
+                        int n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
+
+                        // Break when the end of the file is reached.
+                        if (n == 0)
+                            break;
+
+                        numBytesRead += n;
+                        numBytesToRead -= n;
+                    }
+                    numBytesToRead = bytes.Length;
+
+                    // Write the byte array to the other FileStream.
+                    using (FileStream fsNew = new FileStream(pathNew,
+                        FileMode.Create, FileAccess.Write))
+                    {
+                        fsNew.Write(bytes, 0, numBytesToRead);
+                    }
+                }
+            }
+            catch (FileNotFoundException ioEx)
+            {
+                Console.WriteLine(ioEx.Message);
+            }
+
+        }
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public string[] Detail { get; set; }
+    }
+}
